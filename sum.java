51b3cf2722ce1039be78198d481964193161d5cf
Kishore9239919 <166894418+Kishore9239919@users.noreply.github.com>
2024-04-13 10:37:28 +0530

    Add files via upload
    uploaded my file

---
+++ sum.java
@@ -0,0 +1,10 @@
+import java.utill.*;
+class sumofNumbers
+{
+public static void main(String args[])
+{
+int x=integer.praseInt(args[0]);
+int y=integer.praseInt(args[1]);
+int sum=x+y;
+sytem.out.println("The sum of x and y is:"+sum);
+}
